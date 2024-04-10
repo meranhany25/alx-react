a097bb6e5c25647fb72adb2ef41fd222f847a605
devbn3li <fudwhc@gmail.com>
2024-04-10 12:32:05 +0200

    fix

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
-import Notifications from "../Notifications/Notifications";
-import "./App.css";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
+import React, { Component } from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Login from './Login/Login';
+import CourseList from './CourseList/CourseList';
+import Notifications from './Notifications/Notifications';
+import './App.css';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from './utils/utils';
 
-class App extends React.Component {
+class App extends Component {
   listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
   ];
 
   listNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: getLatestNotification() },
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: getLatestNotification() },
   ];
 
   render() {
